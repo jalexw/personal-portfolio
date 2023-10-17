accf3b19326ad4e91ac836ba5bf55e0ef5ddbf72
jalexw <jalexwhitman@gmail.com>
2023-10-17 13:20:00 -0300

    change link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,6 @@ export default function Home() {
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://jalexw.ca"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             By{' '}
             J. Alex Whitman
