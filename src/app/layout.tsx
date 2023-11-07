c0ded1bdce25b1b03755efa84a7dddbd91917a77
jalexw <jalexwhitman@gmail.com>
2023-11-07 16:10:27 -0400

    import from new metadata folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { description, title } from './metadata'
+import { description, title } from '@/metadata'
 import type { ReactElement, ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
