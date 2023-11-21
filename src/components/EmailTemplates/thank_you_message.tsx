923aa5d58ca9b3fd1345ff109d54034f72428221
jalexw <jalexwhitman@gmail.com>
2023-11-21 17:13:58 -0400

    use friendlier user-facing label

--- src/components/EmailTemplates/thank_you_message.tsx
+++ src/components/EmailTemplates/thank_you_message.tsx
@@ -1,36 +1,42 @@
 import type { FC } from 'react';
 import type { ContactFormData } from '@/components/contact';
+import { messageCategories, messageCategoryGroupLabelMap } from '../contact/message-category';
 
 export const ContactConfirmationEmailTemplate: FC<Readonly<ContactFormData>> = ({
   name,
   email,
   subject,
   message
-}) => (
-  <div>
-    <h1>Hey {name}!</h1>
-    <p>Thank you for reaching out to me! Just confirming that I have received your message. I will get back to your message as soon as possible.</p>
-    <br/>
-    <p>Here is a copy of your message:</p>
-    <table>
-      <tbody>
-        <tr>
-          <td>Name: </td>
-          <td>{name}</td>
-        </tr>
-        <tr>
-          <td>Email: </td>
-          <td>{email}</td>
-        </tr>
-        <tr>
-          <td>Subject: </td>
-          <td>{subject}</td>
-        </tr>
-        <tr>
-          <td>Message: </td>
-          <td>{message}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+}) => {
+  const subjectCategory = messageCategories.find(category => category.value === subject);
+  const subjectCategoryGroupLabel = subjectCategory ? messageCategoryGroupLabelMap[subjectCategory.group] : '';
+
+  return (
+    <div>
+      <h1>Hey {name}!</h1>
+      <p>Thank you for reaching out to me! Just confirming that I have received your message. I will get back to your message as soon as possible.</p>
+      <br/>
+      <p>Here is a copy of your message:</p>
+      <table>
+        <tbody>
+          <tr>
+            <td>Name: </td>
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td>Email: </td>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <td>Subject: </td>
+            <td>{subjectCategoryGroupLabel}</td>
+          </tr>
+          <tr>
+            <td>Message: </td>
+            <td>{message}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
