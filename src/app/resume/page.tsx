cd1e760ae4e1a9b9f97d8ff102dfbba7cf79c333
jalexw <jalexwhitman@gmail.com>
2024-03-24 22:17:40 -0300

    start a resume page (use tailwind to make a resume :) )

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,35 @@
+import type { ReactElement } from "react";
+
+function ResumeHeader() {
+  return (
+    <header
+      className="w-full flex items-center justify-center border-b"
+    >
+      <h1>J. Alex Whitman{'\''}s Resume</h1>
+    </header>
+  )
+}
+
+export default function Resume(): ReactElement {
+  return (
+    <div className="
+      grid place-items-center
+      min-h-screen print:min-h-0
+      bg-gray-400"
+    >
+      <main className="
+        m-4 lg:m-12 print:m-0
+        h-[297mm] w-[210mm] print:h-screen
+        overflow-hidden
+        rounded-md print:rounded-none
+        bg-white
+        p-8
+        shadow-lg print:shadow-none
+        flex flex-col justify-start items-center gap-2
+      ">
+        <ResumeHeader />
+        <p>Coming soon!</p>
+      </main>
+    </div>
+  )
+}
