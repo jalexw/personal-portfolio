095333cb12cacb24c8841efe6962a7a22b21dc2b
jalexw <jalexwhitman@gmail.com>
2024-03-24 22:17:26 -0300

    link to resume page

--- src/app/portfolio.tsx
+++ src/app/portfolio.tsx
@@ -2,8 +2,11 @@
 
 import { DynamicExperience } from "@/components/experience";
 import { HeaderBar } from "@/components/header";
+import { Button } from "@/components/ui/button";
 import { WelcomeMessage } from "@/components/welcome-message";
 import { AnimatePresence } from "framer-motion";
+import { FileText } from "lucide-react";
+import Link from "next/link";
 import type { ReactElement } from "react";
 
 export function PortfolioExperienceContent(): ReactElement {
@@ -12,7 +15,7 @@ export function PortfolioExperienceContent(): ReactElement {
       {/** Contains all user-interactable DOM content (like text that fades in as you scroll) */}
       <div
         id="dom-content"
-        className="w-full h-full fixed top-0 left-0 z-10 flex flex-col justify-start items-center"
+        className="w-full h-full fixed top-0 left-0 z-10 flex flex-col justify-start items-center overflow-y-scroll"
       >
         <HeaderBar />
 
@@ -21,6 +24,17 @@ export function PortfolioExperienceContent(): ReactElement {
           className="w-full overflow-x-hidden flex items-center justify-center z-10"
         >
           <WelcomeMessage />
+        </div>
+
+        <div
+          id="resume-call-to-action-container"
+          className="w-full mt-[80vh] z-10 justify-start items-center flex flex-col grow sm:p-2 md:p-4 lg:p-8 xl:p-12 mb-24"
+        >
+          <Link href="/resume">
+            <Button className="gap-2">
+              <FileText className="w-4 h-4" /> View my resume
+            </Button>
+          </Link>
         </div>
       </div>
       
--- src/components/experience/scene.tsx
+++ src/components/experience/scene.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { PerspectiveCamera, useAnimations, useGLTF } from "@react-three/drei";
+import { PerspectiveCamera } from "@react-three/drei";
 import { useRef, type ReactElement } from "react";
 import { Box } from '@react-three/drei'
-import { useTime } from "framer-motion";
-import { Euler, useFrame } from "@react-three/fiber";
-import { BufferGeometry, Material, Mesh, NormalBufferAttributes, Object3DEventMap } from "three";
+import { useFrame } from "@react-three/fiber";
 
 export function Scene(): ReactElement {
-  // const Penguin = useGLTF('/assets/models/penguin.gltf')
-  // const animations = useAnimations(Penguin.animations, Penguin.scene)
-
   // const model = Penguin.scene.children
   const cubeRef = useRef<any>()
   
