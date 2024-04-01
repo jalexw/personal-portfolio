340075be553c513ec3aba2799d38d6c1d07cd6ac
jalexw <jalexwhitman@gmail.com>
2024-04-01 15:40:01 -0300

    start to fav programming language tierlist

--- src/app/resume/fav_programming_languages.tsx
+++ src/app/resume/fav_programming_languages.tsx
@@ -1,15 +1,67 @@
 import type { ReactElement } from "react";
 import { ResumeSection } from "./resume_section";
+import { Separator } from "@/components/ui/separator";
+
+const tiers = ['😍', '😁', '🙂'] as const;
+type Tier = typeof tiers[number];
+
+interface ProgrammingLanguageRef {
+  tier: Tier;
+  name: string;
+}
+
+const languages = [
+  { tier: '😍', name: 'TypeScript' },
+  { tier: '😍', name: 'Rust' },
+  { tier: '😍', name: 'Python' },
+  { tier: '😁', name: 'C++' },
+  { tier: '😁', name: 'C' },
+  { tier: '😁', name: 'C#' },
+  { tier: '😁', name: 'JavaScript' },
+  { tier: '🙂', name: 'Java' },
+  { tier: '🙂', name: 'PHP' },
+  { tier: '🙂', name: 'Perl' },
+  { tier: '🙂', name: 'R' },
+] as const satisfies ProgrammingLanguageRef[];
+
+interface ProgrammingLanguageTierProps {
+  tier: Tier;
+}
+
+function ProgrammingLanguageTier({ tier }: ProgrammingLanguageTierProps) {
+  const tierLanguages = languages.filter(language => language.tier === tier);
+  
+  return (
+    <li key={tier} className="w-full flex flex-row gap-2">
+      <h3 className="text-lg">{tier}</h3>
+      <Separator orientation="vertical" />
+      <ul className="flex flex-row gap-2">
+        {
+          tierLanguages.map(language => (
+            <li key={language.name}>{language.name}</li>
+          ))
+        }
+      </ul>
+    </li>
+  )
+}
+
+function ProgrammingLanguageTierList() {
+  return (
+    <ol className="list-none flex flex-col w-full gap-2">
+      {
+        tiers.map((tier: Tier) => (
+          <ProgrammingLanguageTier key={tier} tier={tier} />
+        ))
+      }
+    </ol>
+  )
+}
 
 export function FavProgrammingLanguages(): ReactElement {
   return (
     <ResumeSection title="Favorite Programming Languages">
-      <ul className="flex flex-col gap-2">
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>Python</li>
-        <li>Java</li>
-      </ul>
+      <ProgrammingLanguageTierList />
     </ResumeSection>
   );
 }
