3b4fc3080844cc617af0cc631d0722f834dc0b45
jalexw <jalexwhitman@gmail.com>
2024-04-01 22:28:35 -0300

    start to education / certification section

--- src/app/resume/education_and_certifications.tsx
+++ src/app/resume/education_and_certifications.tsx
@@ -1,10 +1,64 @@
 import type { ReactElement } from "react";
 import { ResumeSection } from "./resume_section";
+import Image from "next/image";
+
+interface EducationOrCertificationTileProps {
+  img_src: string;
+  img_alt: string;
+  certification_title: string;
+  certification_issuer: string;
+  certification_date: string;
+}
+
+function EducationTile(props: EducationOrCertificationTileProps) {
+  return (
+    <div className="flex flex-col justify-start items-center grow text-center">
+      <Image
+        width={80}
+        height={80}
+        src={props.img_src}
+        alt={props.img_alt}
+      />
+      <p className="text-xs font-bold text-wrap">{ props.certification_title }</p>
+      <p className="text-xs">{ props.certification_issuer }</p>
+      <p className="text-xs">{ props.certification_date }</p>
+    </div>
+  )
+}
 
 export function EducationAndCertificationsSection(): ReactElement {
   return (
     <ResumeSection title="Education & Certifications">
-      <p>i like to learn stuff</p>
+      <div className="w-full flex flex-row justify-around items-start flex-nowrap gap-2">
+        <EducationTile
+          certification_title="Bachelor of Science"
+          certification_issuer="Western University"
+          certification_date="2022"
+          img_alt="Western University logo"
+          img_src="/images/education/western.png"
+        />
+        <EducationTile
+          certification_title="International Baccalaureate Diploma"
+          certification_issuer="Halifax Grammar School"
+          certification_date="2018"
+          img_alt="International Baccalaureate logo"
+          img_src="/images/education/ib.png"
+        />
+         <EducationTile
+          certification_title="Professional Machine Learning Engineer"
+          certification_issuer="Google Cloud Platform"
+          certification_date="2023"
+          img_alt="Google Cloud Platform logo"
+          img_src="/images/education/gcp.png"
+        />
+         <EducationTile
+          certification_title="Associate Cloud Engineer"
+          certification_issuer="Google Cloud Platform"
+          certification_date="2023"
+          img_alt="Google Cloud Platform logo"
+          img_src="/images/education/gcp.png"
+        />
+      </div>
     </ResumeSection>
   )
 }
