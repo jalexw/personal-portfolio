861dca724384b48691e7f8cfb9d18922e7ea18aa
jalexw <jalexwhitman@gmail.com>
2024-09-17 10:26:37 -0300

    start to script to combine fbx animations into gltf model

--- scripts/add_mixamo_animations_to_gltf.py
+++ scripts/add_mixamo_animations_to_gltf.py
@@ -1,6 +1,16 @@
-from os.path import relpath, join
+from os.path import abspath, join
 import sys
 
+if len(sys.argv) < 3:
+    print("Usage: python3 ./scripts/add_mixamo_animations_to_gltf.py ./public/assets/alex_avatar/animations ./public/assets/alex_avatar/animations")
+    exit(1)
+
 name_of_script = sys.argv[0]
-modelsDir = sys.argv[1] if sys.argv[1] else "../public/assets/alex_avatar/animations"
-animationsDir = sys.argv[2] if sys.argv[1] else "../public/assets/alex_avatar/models"
+animationsDir = sys.argv[1] if sys.argv[1] else "../public/assets/alex_avatar/animations"
+modelsDir = sys.argv[2] if sys.argv[1] else "../public/assets/alex_avatar/models"
+
+animationsDir = abspath(animationsDir)
+modelsDir = abspath(modelsDir)
+
+print(f"Models Directory: {modelsDir}")
+print(f"Animations Directory: {modelsDir}")
