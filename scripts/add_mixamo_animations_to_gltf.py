150ef183eb7da3eaf348e5b8f293c72a718f7fd7
jalexw <jalexwhitman@gmail.com>
2024-09-17 10:15:19 -0300

    add .fbx animations to shared directory for avatar

---
+++ scripts/add_mixamo_animations_to_gltf.py
@@ -0,0 +1,6 @@
+from os.path import relpath, join
+import sys
+
+name_of_script = sys.argv[0]
+modelsDir = sys.argv[1] if sys.argv[1] else "../public/assets/alex_avatar/animations"
+animationsDir = sys.argv[2] if sys.argv[1] else "../public/assets/alex_avatar/models"
--- src/components/experience-loader/experience-asset-definitions.ts
+++ src/components/experience-loader/experience-asset-definitions.ts
@@ -2,11 +2,17 @@ import type { AssetDefinition, ExperienceAssetFileTypes } from "./asset-def";
 
 export const experienceAssetDefinitions = [
   {
-    name: 'avatar',
-    url: "/assets/models/alex_avatar.glb",
-    type: 'gltf'
-  }
-] as const satisfies readonly AssetDefinition<string, string, ExperienceAssetFileTypes>[];
+    name: "avatar",
+    url: "/assets/alex_avatar/models/alex_avatar.glb",
+    type: "gltf",
+  },
+] as const satisfies readonly AssetDefinition<
+  string,
+  string,
+  ExperienceAssetFileTypes
+>[];
 
-export type ExperienceAssetFileNames = typeof experienceAssetDefinitions[number]['name'];
-export type ExperienceAssetFileURLs = typeof experienceAssetDefinitions[number]['url'];
+export type ExperienceAssetFileNames =
+  (typeof experienceAssetDefinitions)[number]["name"];
+export type ExperienceAssetFileURLs =
+  (typeof experienceAssetDefinitions)[number]["url"];
