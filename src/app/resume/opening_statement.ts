6e50c8a46f321b9ed2316dc7aea7a66887bd018a
jalexw <jalexwhitman@gmail.com>
2025-11-19 09:17:35 -0400

    update opening statement

--- src/app/resume/opening_statement.ts
+++ src/app/resume/opening_statement.ts
@@ -2,6 +2,6 @@
 // Proficient in delivering results under tight deadlines.
 
 export const openingStatement: string =
-  "Results-driven developer who excels at making complex ideas accessible to non-technical stakeholders through clear communication and engaging presentations." +
+  "I built my first computer at age 12; I've always had a passion for solving problems and designing systems." +
   " " +
-  "I am looking for a role where I can leverage both technical expertise and strong interpersonal skills to align teams and accelerate project outcomes.";
+  "I am looking for full-time or contract work where I can leverage both technical expertise (5+ years experience building frontend and backend systems) and strong interpersonal skills to accelerate impactful projects.";
