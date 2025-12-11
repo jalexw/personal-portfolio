95cdef32f4b93cdbf5f778b9e765998d84e502a0
jalexw <jalexwhitman@gmail.com>
2025-12-11 14:07:02 -0400

    0.4.75 - fix link to resume in prod

--- src/components/resume-link/resume-link.tsx
+++ src/components/resume-link/resume-link.tsx
@@ -1,18 +1,36 @@
 "use client";
 
-import type { ReactElement } from "react";
+import type { FC, PropsWithChildren, ReactElement } from "react";
 
 import { FileText } from "lucide-react";
-import Link from "next/link";
+import { default as NextLink } from "next/link";
 import { Button } from "@schemavaults/ui";
 import getResumeDocumentHref from "@/lib/getResumeDocumentHref";
 
+interface ILinkProps extends PropsWithChildren {
+  href: string;
+  target: "_self" | "_blank";
+}
+type TLink = FC<ILinkProps>;
+
 export function ResumeLink(): ReactElement {
+  const Link: TLink =
+    process.env.NODE_ENV === "development"
+      ? (props: ILinkProps) => (
+          <NextLink href={props.href} target={props.target}>
+            {props.children}
+          </NextLink>
+        )
+      : (props: ILinkProps) => (
+          <a href={props.href} target={props.target}>
+            {props.children}
+          </a>
+        );
+
   return (
     <Link
       href={getResumeDocumentHref()}
       target={process.env.NODE_ENV === "development" ? "_self" : "_blank"}
-      prefetch={process.env.NODE_ENV === "development"}
     >
       <Button className="flex flex-row justify-start gap-2" variant={"outline"}>
         <span className="block md:hidden">Resume: </span>
